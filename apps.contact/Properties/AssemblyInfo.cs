ad2c704585ecc5933d84ac14f3e80dcaa4e0ec53
Max Toro <maxtoroq@gmail.com>
2013-08-04 15:04:43 -0400

    namespace change

--- apps.contact/Properties/AssemblyInfo.cs
+++ apps.contact/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("maxtoroq.apps.web.contact.dll")]
-[assembly: AssemblyDescription("maxtoroq.apps.web.contact.dll")]
+[assembly: AssemblyTitle("maxtoroq.apps.contact.dll")]
+[assembly: AssemblyDescription("maxtoroq.apps.contact.dll")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliantAttribute(true)]
