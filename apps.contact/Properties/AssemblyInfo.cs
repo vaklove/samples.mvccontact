75ff02214aef694f2c0f5ae1b8c6dcbc318da9c7
Max Toro <maxtoroq@gmail.com>
2014-01-15 19:35:16 -0300

    removed unnecessary files

--- apps.contact/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("maxtoroq.apps.contact.dll")]
-[assembly: AssemblyDescription("maxtoroq.apps.contact.dll")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliantAttribute(true)]
